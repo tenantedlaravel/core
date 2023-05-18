63ab200ee46d04a0f0088f8b5814759da1706090
Ollie <code@ollie.codes>
2023-05-18 15:07:19 +0100

    chore: Bound TenantedManager and TenantProvider to the container, and registered the publishable config

--- src/TenantedServiceProvider.php
+++ src/TenantedServiceProvider.php
@@ -3,17 +3,56 @@ declare(strict_types=1);
 
 namespace Tenanted\Core;
 
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
+use Tenanted\Core\Contracts\TenantProvider;
+use Tenanted\Core\Support\TenantedManager;
 
 class TenantedServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
+        $this->registerBindings();
+    }
 
+    private function registerBindings(): void
+    {
+        // The TenantedManager only requires an instance of Application, so it
+        // can be marked as a singleton without a concrete resolution.
+        $this->app->singleton(TenantedManager::class);
+
+        // When requesting a TenantProvider, we want to return the default
+        // implementation; unless, it was requested with parameters that contain
+        // a 'name'.
+        $this->app->bind(TenantProvider::class, function (Application $app, array $parameters) {
+            return $app->make(TenantedManager::class)->provider($parameters['name'] ?? null);
+        });
     }
 
     public function boot(): void
     {
+        if ($this->app->runningInConsole()) {
+            $this->bootConsole();
+        }
+    }
 
+    private function bootConsole(): void
+    {
+        $this->publishes(
+            [
+                $this->packageDir('config', 'tenanted.php') => config_path('tenanted.php'),
+            ],
+            'config'
+        );
+    }
+
+    /**
+     * @param string ...$paths
+     *
+     * @return string
+     */
+    private function packageDir(string... $paths): string
+    {
+        return basename(__DIR__) . DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, $paths);
     }
 }
